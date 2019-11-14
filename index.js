1ebbf5fc0be24ddb164d305d889cb610192eeb24
Aiman Almureish <aiman.almureish@gmail.com>
2019-11-14 18:17:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 findMatching(driver)
 function findMatching(array, string){
  let arr = array.filter(e => {
-  
+  if(e.includes){
+    
+  }else{
+    
+  }
     return e.includes(string)
   })
   return arr

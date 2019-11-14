551778ccd34f630c7ab613be35757692ce3a48e2
Aiman Almureish <aiman.almureish@gmail.com>
2019-11-14 18:20:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,13 +4,8 @@ const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 findMatching(driver)
 function findMatching(array, string){
  let arr = array.filter(e => {
-  if(e.includes(string.toUpperCase())){
-        return e.includes(string)
-
-  }else{
-        return e.includes(string)
-
-  }
+  
+    return e === string
   })
-  return arr
+  return
 }
